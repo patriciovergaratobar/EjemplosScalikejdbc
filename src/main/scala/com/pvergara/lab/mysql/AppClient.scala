96c5f4cce983f4e17b798fcd0533132b0745b372
patriciovergaratobar <patricio.vergara.tobar@gmail.com>
2018-09-22 15:06:10 -0300

    Add example inner join

--- src/main/scala/com/pvergara/lab/mysql/AppClient.scala
+++ src/main/scala/com/pvergara/lab/mysql/AppClient.scala
@@ -1,4 +1,5 @@
 package com.pvergara.lab.mysql
+import com.pvergara.lab.mysql
 import scalikejdbc._
 import scalikejdbc.config._
 
@@ -63,12 +64,12 @@ object AppClient extends App {
         .update.apply()
   }
   val usersList3: List[UserModel] = withSQL { select.from(UserModel as u) }.map(UserModel(u.resultName)).list.apply()
-  println("REGISTRO ACTUALES")
+  println("\nREGISTRO ACTUALES")
   usersList3 foreach println
 
   //DELETE
   //DELETE FROM `chucrutdb`.`user` WHERE user_id =
-  println("ELIMINAR REGISTROS")
+  println("\nELIMINAR REGISTROS")
   val nameRuben = "ruben"
   val nameCvergara = "cvergara"
   val usersList4: List[UserModel] = withSQL { select.from(UserModel as u).where.eq(u.userName, nameRuben).or.eq(u.userName, nameCvergara) }.map(UserModel(u.resultName)).list.apply()
@@ -78,14 +79,22 @@ object AppClient extends App {
 
 
   //SELECT TABLA VEHICLE
-  println("Jugando con los vehiculos...")
+  println("\nJugando con los vehiculos...")
   val v = VehicleModel.syntax("v")
   val vehiclesList: List[VehicleModel] = withSQL { select.from(VehicleModel as v) }.map(VehicleModel(v.resultName)).list.apply()
   vehiclesList foreach println
 
   //Join entre user y vehicle
-  //.......?
 
+  val (uv, vv) = (UserVehiclesModel.syntax, VehicleModel.syntax)
+
+  //INNER JOIN
+  val userVehiclesList: Seq[UserVehiclesModel] = withSQL {
+    select.from(UserVehiclesModel as uv).innerJoin(VehicleModel as vv).on(uv.id, vv.userId)
+  }.map(UserVehiclesModel(uv, vv)).list.apply()
+
+  println("\nResultado INNER JOIN")
+  userVehiclesList foreach println
 
   DBs.closeAll()
 }
@@ -110,14 +119,7 @@ object UserModel extends SQLSyntaxSupport[UserModel] {
     "^mail$" -> "email"
   )
 
-  /*
-  def apply(rs: WrappedResultSet) = new UserModel(
-
-    rs.int("user_id"),
-    rs.nString("username"), rs.nString("name"), rs.nString("last_name"), rs.int("age"),
-    rs.nString("email"), rs.nString("phono"), rs.nString("perfil")
-  )*/
-
+  def apply(u: SyntaxProvider[UserModel])(rs: WrappedResultSet): UserModel = apply(u.resultName)(rs)
 
   def apply(u: ResultName[UserModel])(rs: WrappedResultSet) = new UserModel(
 
@@ -143,6 +145,10 @@ object VehicleModel extends SQLSyntaxSupport[VehicleModel] {
     "^userId$" -> "user_user_id"
   )
 
+
+
+  def apply(v: SyntaxProvider[VehicleModel])(rs: WrappedResultSet): VehicleModel = apply(v.resultName)(rs)
+
   def apply(v: ResultName[VehicleModel])(rs: WrappedResultSet) = new VehicleModel(
     rs.int(v.vehicleId),
     rs.nString(v.plate),
@@ -151,5 +157,32 @@ object VehicleModel extends SQLSyntaxSupport[VehicleModel] {
     rs.double(v.lastLatitude),
     rs.int(v.userId)
   )
+  /*def opt(v: SyntaxProvider[VehicleModel])(rs: WrappedResultSet): Option[VehicleModel] =
+    rs.longOpt(v.resultName.id).map(_ => VehicleModel(v)(rs))
+*/
+}
+
+case class UserVehiclesModel(id: Int, userName: String, name: String, lastName: String, age: Int, mail: String, phono: String, perfil: String, vehicles: Option[VehicleModel] = None)
+
+object UserVehiclesModel extends SQLSyntaxSupport[UserVehiclesModel] {
+  //Nombre de la tabla
+  override val tableName = "user"
+  //Columnas de la tabla
+  override val columns = Seq("user_id", "username", "name", "last_name", "age", "email", "phono", "perfil")
+  override val nameConverters = Map(
+    "^id$" -> "user_id",
+    "^userName$" -> "username",
+    "^lastName$" -> "last_name",
+    "^mail$" -> "email"
+  )
+  def apply(u: SyntaxProvider[UserVehiclesModel])(rs: WrappedResultSet): UserVehiclesModel = apply(u.resultName)(rs)
+  def apply(u: ResultName[UserVehiclesModel])(rs: WrappedResultSet) = new UserVehiclesModel(
+    rs.get(u.id),
+    rs.get(u.userName), rs.get(u.name), rs.get(u.lastName), rs.get(u.age),
+    rs.get(u.mail), rs.get(u.phono), rs.get(u.perfil)
+  )
+
+  def apply(u: SyntaxProvider[UserVehiclesModel],  v: SyntaxProvider[VehicleModel])(rs: WrappedResultSet):
+  UserVehiclesModel = (apply(u)(rs)).copy(vehicles = Some(VehicleModel(v)(rs)))
 
 }
